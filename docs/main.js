b4d617c5b242c5330574268d8b0344d6b54fdc7e
changmink <changmin043@gmail.com>
2021-02-04 22:51:24 +0900

    [NoIssue] form에서 값 가져오기, login페이지

--- docs/main.js
+++ docs/main.js
@@ -3,4 +3,5 @@ console.log(login)
 login.addEventListener('click', (event)=> {
     event.preventDefault();
     login.textContent = (login.textContent === "login")? "logout" : "login"
+    window.open("./login.html", "_black", 'width=300px,height=300px,toolbars=no,scrollbars=no');
 })
