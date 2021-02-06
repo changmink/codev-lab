3cd5538d7c8ff26f3cc1591383632249895e74ec
changmink <changmin043@gmail.com>
2021-02-06 12:47:18 +0900

    [NoIssue] 자식창에서 부모창으로 값 전달

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,8 @@
 const login = document.querySelector(".menu__login")
 console.log(login)
+let childWindow
 login.addEventListener('click', (event)=> {
     event.preventDefault();
     login.textContent = (login.textContent === "login")? "logout" : "login"
-    window.open("./login.html", "_black", 'width=300px,height=300px,toolbars=no,scrollbars=no');
+    childWindow = window.open("./login.html", "_black", 'width=300px,height=300px,toolbars=no,scrollbars=no');
 })
